22a22a9f82e12af46d45718d7862be94a5e3c5a8
60gCreatine <cphah643@gmail.com>
2025-02-10 15:29:14 +0100

    updated arrest script

--- Arrests/Arrests.R
+++ Arrests/Arrests.R
@@ -83,6 +83,24 @@ ggplot(dfUS_long, aes(x = factor(cluster), y = Value, fill = Crime_Type)) +
                                "UrbanPop" = "#9bc1bc", 
                                "Rape" = "#5D576B"))  
 
+##### Plot clusters on states #####
+library(plotly)
+
+plot_geo(dfUS_summary, locationmode = "USA-states") %>%
+  add_trace(
+    locations = ~state,
+    z = ~cluster,
+    text = ~state,
+    colorscale = "Viridis"
+  ) %>%
+  layout(title = "US States Clustered by Crime Statistics", geo = list(scope = "usa"))
+
+
+
+
+
+
+
 
 #### DK Arrests ####
 dkstraf <- dst_meta(table = "STRAF44", lang = "da")
@@ -96,20 +114,73 @@ straf_filter <- list(
 )
 DanskStraf <- dst_get_data(table = "STRAF44", query = straf_filter, lang = "da")
 
+DanskStraf_clean <- DanskStraf[,-c(1,2,5)]
+DS_wide <- DanskStraf_clean %>%
+  pivot_wider(names_from = OVERTRÆD, values_from = value, values_fill = list(value = 0))
+colnames(DS_wide)[colnames(DS_wide) == "Voldtægt mv."] <- "Rape"
+colnames(DS_wide)[colnames(DS_wide) == "Voldsforbrydelser i alt"] <- "Assault"
+colnames(DS_wide)[colnames(DS_wide) == "Manddrab"] <- "Murder"
 
-# dkby2 <- dst_meta(table = "BY2", lang = "da")
-# by2_filter <- list(
-#   Tid=c("2023", "2022", "2021", "2020"),
-#   KOMK="*",
-#   BYST="*",
-#   ALDER="*",
-#   KØN="*")
-# DanskBY2 <- dst_get_data(table = "BY2", query = by2_filter, lang = "da")
+DS_summary <- DS_wide %>%
+  mutate(total = as.numeric(Rape + Assault + Murder)) %>%  
+  arrange(desc(total)) 
 
-BY2 <- read.csv("BY2.csv", sep = ";")
+# Outliars
+DS_summary <- DS_summary[DS_summary$OMRÅDE != "Hele landet", ]
+DS_summary <- DS_summary[!grepl("^Region ", DS_summary$OMRÅDE), ]
+DS_summary <- DS_summary[DS_summary$OMRÅDE != "København", ]
+
+
+DS_scale = as.data.frame(scale(DS_summary[2:4]))
+DS_cluster <- kmeans(DS_scale, centers = 3, nstart = 10)
+
+DS_elbow=data.frame(k=1:20,twss=0)
+for (i in (1:20)) {
+  tmod=kmeans(DS_scale,centers = i,nstart = 10)
+  DS_elbow[i,'twss']=tmod$tot.withinss
+}
+plot(DS_elbow)
+
+DK_kmres=kmeans(DS_scale, centers = 3, nstart = 10)
+fviz_cluster(DS_cluster,DS_scale)
+
+DS_summary$cluster <- DS_cluster$cluster
+
+##### Kort #####
+
+
+
+
+ dkby2 <- dst_meta(table = "BY2", lang = "da")
+ by2_filter <- list(
+   Tid=c("2023"),
+   KOMK="*",
+   BYST="*")
+ BY2 <- dst_get_data(table = "BY2", query = by2_filter, lang = "da")
+
+#BY2 <- read.csv("Arrests/BY2.csv", sep = ";")
+BY2 <- BY2[,-1]
 BY2 <- BY2 %>% 
-  filter(as.numeric(INDHOLD) > 0)
-colnames(DanskStraf)[colnames(DanskStraf) == "OMRÅDE"] <- "KOMK"
+  filter(as.numeric(value) > 0)
+
+BY2s <- BY2 %>%
+  group_by(KOMK) %>%
+  summarise(
+    total_value = sum(value),  # Total
+    land_value = sum(value[BYST == "Landdistrikter"]),  # antallet under Landdistrikter
+    land_share = round((land_value / total_value) * 100,2)  # beregn landdistrikter
+  ) %>%
+  arrange(desc(total_value))
+
+
+
+
+
+
+
+
+
+
 
 BY_Vold <- merge(BY2, DanskStraf, "KOMK")
 BY_Vold <- BY_Vold %>% 
