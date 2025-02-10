09df11d8c5a7e8d9db2926cec06b5bda2084f4c5
60gCreatine <cphah643@gmail.com>
2025-02-10 23:50:27 +0100

    read.csv for Brønby

---
+++ SoccerData/Brønby.R
@@ -0,0 +1,7 @@
+
+df <- read.csv("SoccerData/Brønby_data/matchesbif.csv")
+
+allbifmatchPasses <- read.csv("SoccerData/Brønby_data/eventspasses.csv")
+allbifmatchShot <- read.csv("SoccerData/Brønby_data/eventsshots.csv")
+allbifmatchPlayers <- read.csv("SoccerData/Brønby_data/matchdetailplayers.csv")
+
